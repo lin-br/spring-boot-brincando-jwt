78415447fb344ec3bcb100b2e5c35f291824171f
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-03-11 12:50:15 -0300

    Implementar tratamento de exception para jwt expirado

--- src/main/java/xyz/tilmais/brincandojwt/security/FiltroAutenticacao.java
+++ src/main/java/xyz/tilmais/brincandojwt/security/FiltroAutenticacao.java
@@ -1,5 +1,8 @@
 package xyz.tilmais.brincandojwt.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -9,7 +12,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FiltroAutenticacao extends GenericFilterBean {
     @Override
@@ -18,9 +23,20 @@ public class FiltroAutenticacao extends GenericFilterBean {
                          FilterChain chain)
             throws IOException, ServletException {
 
-        Authentication authentication = TokenAutenticacao.obterAutenticacao((HttpServletRequest) request);
+        try {
+            Authentication authentication = TokenAutenticacao.obterAutenticacao((HttpServletRequest) request);
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        chain.doFilter(request, response);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            chain.doFilter(request, response);
+        } catch (ExpiredJwtException jwtException) {
+            String message = "Token vencido ou inválido!";
+            logger.info(message);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+            httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            httpServletResponse.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+            httpServletResponse.getWriter().write("{\"mensagem\":\"" + message + "\"}");
+        }
     }
 }
